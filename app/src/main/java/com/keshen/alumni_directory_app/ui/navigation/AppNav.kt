aa65e16631f669388307df18c87fa8f0ca53b51e
Keshen <njkeshen98@gmail.com>
2025-12-22 15:36:31 +0800

    Create sign in and sign up screen files and update appnav and screens file under navigation

--- app/src/main/java/com/keshen/alumni_directory_app/ui/navigation/AppNav.kt
+++ app/src/main/java/com/keshen/alumni_directory_app/ui/navigation/AppNav.kt
@@ -4,6 +4,8 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.keshen.alumni_directory_app.ui.screens.auth.SignInScreen
+import com.keshen.alumni_directory_app.ui.screens.auth.SignUpScreen
 import com.keshen.alumni_directory_app.ui.screens.home.HomeScreen
 
 @Composable
@@ -12,11 +14,18 @@ fun AppNav() {
 
     NavHost(
         navController = navController,
-        startDestination = Screen.Home,
+        startDestination = Screen.SignIn,
 
     ) {
         composable<Screen.Home> {
             HomeScreen(navController)
+        }
+        composable<Screen.SignIn> {
+            SignInScreen(navController)
+        }
+
+        composable<Screen.SignUp> {
+            SignUpScreen(navController)
         }
     }
 }
---
+++ app/src/main/java/com/keshen/alumni_directory_app/ui/screens/auth/SignUpScreen.kt
@@ -0,0 +1,9 @@
+package com.keshen.alumni_directory_app.ui.screens.auth
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun SignUpScreen(navController: NavHostController) {
+
+}
